3c5d8b299ba60d8942e420ecb4480914af10a5b4
giococ <gionata.coccia@assist.it>
2017-12-20 17:00:37 +0100

    added hapi server

---
+++ server.js
@@ -0,0 +1,16 @@
+'use strict';
+
+const Hapi = require('hapi');
+
+// Create a server with a host and port
+const server = new Hapi.Server();
+server.connection({host: 'localhost', port: 3000});
+
+
+server.start((err)=> {
+
+    if (err) {
+        throw err;
+    }
+    console.log('Server running at:', server.info.uri);
+});
